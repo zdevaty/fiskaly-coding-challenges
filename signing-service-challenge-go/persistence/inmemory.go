50fab06c324e5d887e98f9ca30ed7252159e3017
Zdenek Devaty <zdenek.devaty@eliona.io>
2025-07-02 17:46:46 +0200

    Implement persistence

--- signing-service-challenge-go/persistence/inmemory.go
+++ signing-service-challenge-go/persistence/inmemory.go
@@ -1,3 +1,45 @@
 package persistence
 
-// TODO: in-memory persistence ...
+import (
+	"errors"
+
+	"github.com/zdevaty/fiskaly-coding-challenges/signing-service-challenge/domain"
+)
+
+var (
+	ErrDeviceNotFound = errors.New("device not found")
+	ErrDeviceExists   = errors.New("device already exists")
+)
+
+type DeviceStore interface {
+	Create(device domain.SignatureDevice) error
+	Get(id string) (domain.SignatureDevice, error)
+}
+
+type InMemoryDeviceStore struct {
+	devices map[string]domain.SignatureDevice
+}
+
+func NewInMemoryDeviceStore() *InMemoryDeviceStore {
+	return &InMemoryDeviceStore{
+		devices: make(map[string]domain.SignatureDevice),
+	}
+}
+
+func (s *InMemoryDeviceStore) Create(device domain.SignatureDevice) error {
+	if _, exists := s.devices[device.ID]; exists {
+		return ErrDeviceExists
+	}
+
+	s.devices[device.ID] = device
+	return nil
+}
+
+func (s *InMemoryDeviceStore) Get(id string) (domain.SignatureDevice, error) {
+	device, exists := s.devices[id]
+	if !exists {
+		return domain.SignatureDevice{}, ErrDeviceNotFound
+	}
+
+	return device, nil
+}
