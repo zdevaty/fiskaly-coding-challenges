1b85f5e1c386691104f8ca000836a287e4d90b39
Zdenek Devaty <zdenek.devaty@eliona.io>
2025-07-03 15:50:39 +0200

    Implement key generation

--- signing-service-challenge-go/api/device.go
+++ signing-service-challenge-go/api/device.go
@@ -1,10 +1,15 @@
 package api
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rsa"
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
 	"net/http"
 
+	"github.com/zdevaty/fiskaly-coding-challenges/signing-service-challenge/crypto"
 	"github.com/zdevaty/fiskaly-coding-challenges/signing-service-challenge/domain"
 )
 
@@ -28,9 +33,36 @@ func (s *Server) CreateSignatureDevice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// todo: verify algorithm
-	var publicKey, privateKey []byte
-	// todo: generate keys
+	var (
+		publicKey  []byte
+		privateKey []byte
+	)
+
+	switch req.Algorithm {
+	case "ECC":
+		generator := crypto.ECCGenerator{}
+		keypair, err := generator.Generate()
+		if err != nil {
+			http.Error(w, "key generation failed: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		publicKey = marshalECCPublicKey(keypair.Public)
+		privateKey = marshalECCPrivateKey(keypair.Private)
+
+	case "RSA":
+		generator := crypto.RSAGenerator{}
+		keypair, err := generator.Generate()
+		if err != nil {
+			http.Error(w, "key generation failed: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		publicKey = marshalRSAPublicKey(keypair.Public)
+		privateKey = marshalRSAPrivateKey(keypair.Private)
+
+	default:
+		http.Error(w, "unsupported algorithm", http.StatusBadRequest)
+		return
+	}
 
 	device := domain.SignatureDevice{
 		ID:         req.ID,
@@ -39,7 +71,6 @@ func (s *Server) CreateSignatureDevice(w http.ResponseWriter, r *http.Request) {
 		PublicKey:  publicKey,
 		PrivateKey: privateKey,
 	}
-
 	if err := s.store.Create(device); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -47,11 +78,30 @@ func (s *Server) CreateSignatureDevice(w http.ResponseWriter, r *http.Request) {
 
 	response := createSignatureDeviceResponse{
 		ID:        device.ID,
-		Algorithm: string(device.Algorithm),
+		Algorithm: device.Algorithm,
 		Label:     device.Label,
 		PublicKey: base64.StdEncoding.EncodeToString(device.PublicKey),
 	}
 
+	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
+}
+
+func marshalECCPublicKey(pub *ecdsa.PublicKey) []byte {
+	return elliptic.Marshal(pub.Curve, pub.X, pub.Y)
+}
+
+func marshalECCPrivateKey(priv *ecdsa.PrivateKey) []byte {
+	keyBytes, _ := x509.MarshalECPrivateKey(priv)
+	return keyBytes
+}
+
+func marshalRSAPublicKey(pub *rsa.PublicKey) []byte {
+	keyBytes, _ := x509.MarshalPKIXPublicKey(pub)
+	return keyBytes
+}
+
+func marshalRSAPrivateKey(priv *rsa.PrivateKey) []byte {
+	return x509.MarshalPKCS1PrivateKey(priv)
 }
